84f79a54642cc4398d7f02d9a08bfd1a3998b3e5
pedro <pedro311@gmail.com>
2024-08-29 12:29:06 +0200

    clean-up

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -19,11 +19,7 @@ rm -f $TOOLCHAIN/lib/libiberty.a
 rm -f $TOOLCHAIN/lib/libc.a
 
 cp -f $FILES/fixed/in.h $SYSROOT/usr/include/netinet/in.h
-#cp -f $FILES/namespaces/if_link.h $SYSROOT/usr/include/linux/if_link.h
-#cp -f $FILES/namespaces/sysnum.h $SYSROOT/usr/include/bits/sysnum.h
-#cp -f $FILES/namespaces/unistd.h $SYSROOT/usr/include/asm-generic/unistd.h
 
-#cp -f $FILES/newer/ctype.h $SYSROOT/usr/include/ctype.h
 cp -f $FILES/newer/if_pppol2tp.h $SYSROOT/usr/include/linux/if_pppol2tp.h
 cp -f $FILES/newer/if_pppox.h $SYSROOT/usr/include/linux/if_pppox.h
 cp -f $FILES/newer/timex.h $SYSROOT/usr/include/sys/timex.h
