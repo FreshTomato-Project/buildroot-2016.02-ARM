d9789eba070fac4132f17ceb61fff720544e264d
pedro <pedro311@gmail.com>
2024-08-29 18:44:55 +0200

    Add error handling in build-toolchain.sh script

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -3,29 +3,33 @@
 make clean
 make
 
-FILES=./dl_save/files
-HERE=$(pwd)
-TOOLCHAIN=./output/hndtools-arm-uclibc-5.3/usr
-SYSROOT=$TOOLCHAIN/arm-brcm-linux-uclibcgnueabi/sysroot
+ret=$?
+[ $ret -ne 0 ] && {
+	echo -e "\nAn error occurred while building the toolchain!\n\n"
+} || {
+	FILES=./dl_save/files
+	HERE=$(pwd)
+	TOOLCHAIN=./output/hndtools-arm-uclibc-5.3/usr
+	SYSROOT=$TOOLCHAIN/arm-brcm-linux-uclibcgnueabi/sysroot
 
-cd $TOOLCHAIN
-ln -sf arm-brcm-linux-uclibcgnueabi arm-linux
-ln -sf arm-linux/sysroot/usr usr
-cd $HERE
+	cd $TOOLCHAIN
+	ln -sf arm-brcm-linux-uclibcgnueabi arm-linux
+	ln -sf arm-linux/sysroot/usr usr
+	cd $HERE
 
-# still needed?
-rm -f $TOOLCHAIN/lib/libgmp.*
-rm -f $TOOLCHAIN/lib/libiberty.a
-rm -f $TOOLCHAIN/lib/libc.a
+	# still needed?
+	rm -f $TOOLCHAIN/lib/libgmp.*
+	rm -f $TOOLCHAIN/lib/libiberty.a
+	rm -f $TOOLCHAIN/lib/libc.a
 
-cp -f $FILES/fixed/in.h $SYSROOT/usr/include/netinet/in.h
+	cp -f $FILES/fixed/in.h $SYSROOT/usr/include/netinet/in.h
+	cp -f $FILES/newer/if_pppol2tp.h $SYSROOT/usr/include/linux/if_pppol2tp.h
+	cp -f $FILES/newer/if_pppox.h $SYSROOT/usr/include/linux/if_pppox.h
+	cp -f $FILES/newer/timex.h $SYSROOT/usr/include/sys/timex.h
+	cp -f $FILES/pps/timepps.h $SYSROOT/usr/include/timepps.h
+	cp -f $FILES/wireguard/netlink.h $SYSROOT/usr/include/linux/netlink.h
 
-cp -f $FILES/newer/if_pppol2tp.h $SYSROOT/usr/include/linux/if_pppol2tp.h
-cp -f $FILES/newer/if_pppox.h $SYSROOT/usr/include/linux/if_pppox.h
-cp -f $FILES/newer/timex.h $SYSROOT/usr/include/sys/timex.h
+	echo -e "\nToolchain successfully built!\n\n"
+}
 
-cp -f $FILES/pps/timepps.h $SYSROOT/usr/include/timepps.h
-
-cp -f $FILES/wireguard/netlink.h $SYSROOT/usr/include/linux/netlink.h
-
-echo -e "\nToolchain successfully built!\n\n"
+exit $ret
