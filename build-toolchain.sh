5dbeb4b03ce6c2475ee7ac287612656c851aa5c8
pedro <pedro311@gmail.com>
2024-09-11 20:42:18 +0200

    Update build script according to already added patches

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -23,11 +23,7 @@ ret=$?
 	rm -f $TOOLCHAIN/lib/libc.a
 
 	cp -f $FILES/fixed/in.h $SYSROOT/usr/include/netinet/in.h
-	cp -f $FILES/newer/if_pppol2tp.h $SYSROOT/usr/include/linux/if_pppol2tp.h
-	cp -f $FILES/newer/if_pppox.h $SYSROOT/usr/include/linux/if_pppox.h
-	cp -f $FILES/newer/timex.h $SYSROOT/usr/include/sys/timex.h
 	cp -f $FILES/pps/timepps.h $SYSROOT/usr/include/timepps.h
-	cp -f $FILES/wireguard/netlink.h $SYSROOT/usr/include/linux/netlink.h
 
 	echo -e "\nToolchain successfully built!\n\n"
 }
